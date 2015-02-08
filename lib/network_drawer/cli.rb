4477913a84510be020e7b526459c7d92f2e75921
Hiroshi Ota <otahi.pub@gmail.com>
2015-02-09 07:15:22 +0900

    Change output filename

--- lib/network_drawer/cli.rb
+++ lib/network_drawer/cli.rb
@@ -7,7 +7,7 @@ module NetworkDrawer
     desc 'draw SOURCE', 'draw network diagram with SOURCE file'
     def draw(source_file)
       src = Source.read(source_file)
-      dest_file = source_file
+      dest_file = source_file.gsub(File.extname(source_file), '')
       Diagram.draw(src, dest_file)
     end
   end
