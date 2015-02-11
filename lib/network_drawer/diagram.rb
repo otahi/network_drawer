03c5dec8832efb6a1fd1c033931118b056858817
Hiroshi Ota <otahi.pub@gmail.com>
2015-02-12 07:40:41 +0900

    Refactor methods

--- lib/network_drawer/diagram.rb
+++ lib/network_drawer/diagram.rb
@@ -17,15 +17,15 @@ module NetworkDrawer
       @title = @options[:title] ? @options[:title] :
         File.basename(@dest_file, '.*')
       @nodes = {}
-      @layers = []
+      @layers = {}
       @gv = Gviz.new(@title)
     end
 
     def draw
       @gv.global(rankdir: 'TB')
 
-      create_nodes
       create_layers
+      create_nodes(@source[:nodes])
       create_connections
 
       @gv.save @dest_file, :svg
@@ -33,39 +33,41 @@ module NetworkDrawer
 
     private
 
-    def create_nodes
-      @source[:nodes].each_with_index do |s, i|
-        id = "#{i}".to_sym
-        name = s[:name]
-        ports = s[:ports]
-        layer = s[:layer] ? s[:layer] : :default
-        label = '<table>'
-        @layers << layer unless @layers.include?(layer)
+    def create_layers
+      @layers.merge!(@source[:layers]) if @source[:layers]
+      @layers.each_pair do |l, n|
+        create_nodes(n[:nodes], l)
+      end
+    end
 
-        if ports
+    def create_nodes(nodes, layer = nil)
+      nodes.reverse.each_with_index do |s, i|
+        id = "#{@nodes.size + 1}".to_sym
+        name = s.keys.first
+        ports = s[name][:ports] ? s[name][:ports] : []
+        label = '<table>'
+
+        if ports.empty?
+          label << "<tr><td>#{name}</td></tr>"
+        else
           label << '<tr>'
           ports.each_with_index do |p, j|
             label << "<td port=\"p#{p.gsub('/', '')}\"> #{p} </td>"
           end
           label << '</tr>'
+          label << "<tr><td colspan=\"#{ports.size}\">#{name}</td></tr>"
         end
-        label << "<tr><td colspan=\"#{ports.size}\">#{name}</td></tr>"
+        label << '</table>'
         @nodes.merge!(name =>
           { id: id, label: label, ports: ports, layer: layer })
-        label << '</table>'
-      end
-    end
 
-    def create_layers
-      @layers = @layers + @source[:layers] if @source[:layers]
-
-      @layers.each do |l|
-        l_nodes = @nodes.select { |_, v| v[:layer] == l }
-        @gv.subgraph do
-          global label: l
-          l_nodes.each_value do |n|
-            node n[:id], label: n[:label], shape: 'plaintext'
+        if layer
+          @gv.subgraph "cluster_#{layer}"do
+            global label: layer
+            node id, label: label, shape: 'plaintext'
           end
+        else
+          @gv.node id, label: label, shape: 'plaintext'
         end
       end
     end
@@ -75,8 +77,8 @@ module NetworkDrawer
         from_name, from_port  = c[:from].to_s.split(':')
         to_name, to_port = c[:to].to_s.split(':')
 
-        from_id = @nodes[from_name][:id]
-        to_id = @nodes[to_name][:id]
+        from_id = @nodes[from_name.to_sym][:id]
+        to_id = @nodes[to_name.to_sym][:id]
 
         from = from_port ? "#{from_id}:p#{from_port}" : from_id
         to = to_port ? "#{to_id}:p#{to_port}" : to_id
