8d7047a18adbd8fd0d16bb6579a6d10ec4e93082
Hiroshi Ota <otahi.pub@gmail.com>
2015-03-18 06:48:22 +0900

    Fix to show layer cluster

--- lib/network_drawer/element/layer.rb
+++ lib/network_drawer/element/layer.rb
@@ -4,17 +4,36 @@ module NetworkDrawer
   module Element
     # Replesent of layer
     class Layer < Element
+      DEFAULT_STYLE = { fontname: 'Helvetica' }
+
+      def initialize(initial_values = {}, style = {})
+        super
+        @default_style = DEFAULT_STYLE
+      end
+
       def to_code
         node_code = ''
+
+        style = style(self.type).dup
+        style.merge!(self.to_hash)
+        style.delete(:layers)
+        style.delete(:nodes)
+        style.delete(:connections)
+        label = self.name unless Diagram::TOP_LAYER == self.name
+
         nodes.each { |n| node_code += n.to_code + "\n" } if nodes
         layer_code = ''
         layers.each { |l| layer_code += l.to_code + "\n" } if layers
-        <<-EOF
-        #{node_code}
-        subgraph do
+        code = <<-EOF
+        subgraph "cluster_#{self.name}" do
+          global label: "#{label}"
+          global(#{style})
+          #{node_code}
           #{layer_code}
         end
         EOF
+
+        code
       end
     end
   end
