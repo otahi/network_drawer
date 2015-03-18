6573157b6df98eaee4e231417e31a0f9e2d5346c
Hiroshi Ota <otahi.pub@gmail.com>
2015-03-19 06:50:09 +0900

    Add symbolize yaml file

--- lib/network_drawer/source.rb
+++ lib/network_drawer/source.rb
@@ -10,9 +10,10 @@ module NetworkDrawer
       when :json
         source = JSON.parse(File.read(file_name), symbolize_names: true)
       when :yaml
-        source = YAML.load(File.read(file_name))
+        source = symbolize(YAML.load(File.read(file_name)))
       else
         puts 'Incorrect file type'
+        exit 1
       end
       source
     end
@@ -27,6 +28,17 @@ module NetworkDrawer
       else
         nil
       end
+    end
+    def self.symbolize(obj)
+      return obj.inject({}) do |memo, (k, v)|
+        memo[k.to_sym] =  symbolize(v)
+        memo
+      end if obj.is_a? Hash
+      return obj.inject([]) do |memo, v|
+        memo << symbolize(v)
+        memo
+      end if obj.is_a? Array
+      return obj
     end
   end
 end
