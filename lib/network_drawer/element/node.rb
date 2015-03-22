f8d1a4abfb91628f83bce17753478344799bdd5e
Hiroshi Ota <otahi.pub@gmail.com>
2015-03-23 06:10:51 +0900

    Add module to node like port

--- lib/network_drawer/element/node.rb
+++ lib/network_drawer/element/node.rb
@@ -22,17 +22,58 @@ module NetworkDrawer
       private
 
       def build_label
-        if self.ports && self.ports.size > 0
-          label = "<tr border='1'>"
+        num_column = [num_ports, num_modules, 1].max
+        label = ''
+
+        label << port_label
+        if [num_ports, num_modules].max > 0
+          label << "<tr border='1'><td border='1' colspan=\"#{num_column}\">#{self.name}</td></tr>"
+        else
+          label << "<tr border='0'><td border='0'>#{self.name}</td></tr>"
+        end
+        label << module_label
+
+        "<table border='0'>#{label}</table>"
+      end
+
+      def port_label
+        label = ''
+        if num_ports > 0
+          label << "<tr border='1'>"
           self.ports.each_with_index do |p, j|
             label << "<td border='1' port=\"p#{p.gsub('/', '')}\">#{p}</td>"
           end
           label << '</tr>'
-          label << "<tr border='1'><td border='1' colspan=\"#{self.ports.size}\">#{self.name}</td></tr>"
-        else
-          label = "<tr border='1'><td>#{self.name}</td></tr>"
         end
-        "<table border='0'>#{label}</table>"
+        label
+      end
+
+      def module_label
+        label = ''
+        if num_modules > 0
+          label << "<tr border='1'>"
+          self.modules.each_with_index do |p, j|
+            label << "<td border='1' port=\"p#{p.gsub('/', '')}\">#{p}</td>"
+          end
+          label << '</tr>'
+        end
+        label
+      end
+
+      def num_ports
+        if self.ports && self.ports.respond_to?(:size)
+          self.ports.size
+        else
+          0
+        end
+      end
+
+      def num_modules
+        if self.modules && self.modules.respond_to?(:size)
+          self.modules.size
+        else
+          0
+        end
       end
     end
   end
